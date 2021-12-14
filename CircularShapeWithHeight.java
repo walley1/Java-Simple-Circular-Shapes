d8341777c457436dd95caf277b442d24eded03f3
Austin <69608231+walley1@users.noreply.github.com>
2021-12-13 20:15:41 -0500

    Update CircularShapeWithHeight.java

--- CircularShapeWithHeight.java
+++ CircularShapeWithHeight.java
@@ -1,5 +1,4 @@
 /**
- * Add class comment and @tags
 * This class will model an abstract superclass of a generic circular shape that has a radius and a height. 
 *
 * @author Austin Walley
@@ -8,63 +7,36 @@
 
 public abstract class CircularShapeWithHeight 
 {
-    /**
-    * Declare and initialize 2 instance variables.
-    */
+
     private double radius;
     private double height;
         
-    /**
-    * Create a constructor.
-    * @param radius of radius.
-    * @param height of height.
-    */
     public CircularShapeWithHeight(double radius,double height)
     {
         this.radius = radius;
         this.height = height;
     }
         
-    /**
-    * Accessor method that returns diameter.
-    * @return radius
-    */
     public double getDiameter()
     {
         return this.radius*2;
     }
 
-    /**
-    * Accessor method that returns radius.
-    * @return radius
-    */
     public double getRadius()
     {
         return this.radius;
     }
         
-    /**
-    * Accessor method that returns height.
-    * @return height
-    */
     public double getHeight()
     {
         return this.height;
     }
 
-    /**
-    * Accessor method that returns the cross section area.
-    * @return 3.14159265 * radius * radius
-    */
     public double getCrossSectionArea()
     {
         return 3.14159265 * radius * radius;
     }
 
-    /**
-    * Accessor method that returns the cross section perimeter.
-    * @return 2 * 3.14159265 * radius
-    */
     public double getCrossSectionPerimeter()
     {
         return 2 * 3.14159265 * radius;
