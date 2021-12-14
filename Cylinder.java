c0342647d54dae06e9fe7608917d0275d147c8ae
Austin <69608231+walley1@users.noreply.github.com>
2021-12-13 20:13:59 -0500

    Add files via upload

---
+++ Cylinder.java
@@ -0,0 +1,20 @@
+/**
+ * Add class comment and @tags
+* This class will model a Cylinder 3D shape.
+*
+* @author Austin Walley
+* @version 10/3/21
+ */
+
+public class Cylinder extends CircularShapeWithHeight 
+{
+    /**
+    * Constructor that takes radius and height.
+    * @param radius of radius.
+    * @param height of height.
+    */          
+    public Cylinder(double radius,double height)
+    {
+        super(radius,height);
+    }
+}
